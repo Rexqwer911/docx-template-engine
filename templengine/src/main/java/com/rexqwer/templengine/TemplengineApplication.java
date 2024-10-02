aa0bd60ace68111809696e7f6ebc45933518d963
Rexqwer911 <rexqwer911@gmail.com>
2024-10-03 00:24:59 +0300

    project initialization

---
+++ templengine/src/main/java/com/rexqwer/templengine/TemplengineApplication.java
@@ -0,0 +1,13 @@
+package com.rexqwer.templengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemplengineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TemplengineApplication.class, args);
+    }
+
+}
---
+++ templengine/src/test/java/com/rexqwer/templengine/TemplengineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rexqwer.templengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplengineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
